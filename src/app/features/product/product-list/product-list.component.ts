7dcb10d012a012f910cdaa1efab00b621fca1c73
janaiscoding <jana.istrate@gmail.com>
2024-06-05 20:04:40 +0200

    Add icon library, fav button

--- src/app/features/product/product-list/product-list.component.ts
+++ src/app/features/product/product-list/product-list.component.ts
@@ -1,13 +1,16 @@
 import { Component, OnInit } from "@angular/core";
+import { NgIconComponent, provideIcons } from "@ng-icons/core";
 import { ProductService } from "../product.service";
 import { Product } from "../../../shared/models/product.model";
 import { CommonModule } from "@angular/common";
 import { RouterLink } from "@angular/router";
+import { radixHeart, radixHeartFilled } from "@ng-icons/radix-icons";
 
 @Component({
   selector: "app-product-list",
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, NgIconComponent],
+  viewProviders: [provideIcons({ radixHeart, radixHeartFilled })],
   templateUrl: "./product-list.component.html",
   styleUrl: "./product-list.component.scss",
 })
@@ -18,5 +21,9 @@ export class ProductListComponent implements OnInit {
 
   ngOnInit() {
     this.products = this.productService.fetchAllProducts();
+  }
+
+  onAddToFavorite(product: Product) {
+    product.isFavorite = !product.isFavorite;
   }
 }
--- src/app/shared/models/product.model.ts
+++ src/app/shared/models/product.model.ts
@@ -4,4 +4,5 @@ export interface Product {
   url: string;
   price: number;
   description?: string;
+  isFavorite?: boolean;
 }
