5ee9c0ca34458755d42c6fd63464c0959b225995
janaiscoding <jana.istrate@gmail.com>
2024-06-07 22:03:14 +0200

    Add item component and total

--- src/app/features/cart/cart-item/cart-item.component.ts
+++ src/app/features/cart/cart-item/cart-item.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from "@angular/core";
+import { CartItem } from "../../../shared/models/cart.model";
 
 @Component({
-  selector: 'app-cart-item',
+  selector: "app-cart-item",
   standalone: true,
   imports: [],
-  templateUrl: './cart-item.component.html',
-  styleUrl: './cart-item.component.scss'
+  templateUrl: "./cart-item.component.html",
+  styleUrl: "./cart-item.component.scss",
 })
 export class CartItemComponent {
-
+  @Input() item!: CartItem;
 }
--- src/app/features/cart/cart.component.ts
+++ src/app/features/cart/cart.component.ts
@@ -1,16 +1,19 @@
 import { Component, OnInit } from "@angular/core";
 import { CartService } from "./cart.service";
 import { CartItem } from "../../shared/models/cart.model";
+import { CommonModule } from "@angular/common";
+import { CartItemComponent } from "./cart-item/cart-item.component";
 
 @Component({
   selector: "app-cart",
   standalone: true,
-  imports: [],
+  imports: [CommonModule, CartItemComponent],
   templateUrl: "./cart.component.html",
   styleUrl: "./cart.component.scss",
 })
 export class CartComponent implements OnInit {
   cart?: CartItem[];
+  totalCost = 0;
 
   constructor(private cartService: CartService) {}
 
@@ -18,5 +21,9 @@ export class CartComponent implements OnInit {
     this.cart = this.cartService.fetchCart();
 
     console.log("this.cart: ", this.cart);
+    this.totalCost = this.cart.reduce(
+      (prev, curr) => prev + curr.quantity * curr.price,
+      0
+    );
   }
 }
