838ce0de08e2b70a361ca43c57507c5ceeb924fd
janaiscoding <jana.istrate@gmail.com>
2024-06-11 18:57:49 +0200

    Fix cart.component for new Subscription logic

--- src/app/features/cart/cart.component.ts
+++ src/app/features/cart/cart.component.ts
@@ -4,6 +4,7 @@ import { CommonModule } from "@angular/common";
 import { CartItemComponent } from "./cart-item/cart-item.component";
 import { CartItem } from "../../shared/models/cart.model";
 import { ActivatedRoute, Router } from "@angular/router";
+import { Subscription } from "rxjs";
 
 @Component({
   selector: "app-cart",
@@ -16,12 +17,21 @@ export class CartComponent implements OnInit {
   cart?: any;
   totalCost = 0;
 
+  private sub = new Subscription();
+
   constructor(private cartService: CartService) {}
 
   ngOnInit(): void {
-    this.cart = this.cartService.fetchCart();
-
+    this.sub.add(
+      this.cartService.fetchCart().subscribe((cartItems) => {
+        this.cart = cartItems;
+      })
+    );
     console.log("this.cart: ", this.cart);
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
   }
 
   onPurchase() {
